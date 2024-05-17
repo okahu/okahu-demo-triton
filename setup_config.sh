5d15db786e898ed3db45a4d1c9e5f266b1a2fc44
Prasad Mujumdar <prasad@okahu.ai>
2024-05-17 09:42:41 -0700

    Update docker setup

--- setup_config.sh
+++ setup_config.sh
@@ -6,6 +6,4 @@ if [[ $# -lt 1 ]]; then
 fi
 
 echo Configuring chatbot apps with local Triton inference server
-sed 's/OKAHU_API_KEY=/OKAHU_API_KEY='"$1"'/g ; s/TRITON_LLM_ENDPOINT=/TRITON_LLM_ENDPOINT=http:\/\/127.0.0.1:8000\/v2\/models\/flan_t5\/infer/g ; s/TRITON_LLM_GRPC_ENDPOINT=/TRITON_LLM_GRPC_ENDPOINT=http:\/\/127.0.0.1:8001' config/config.ini.template > config/config.ini 
-TRITON_LLM_GRPC_ENDPOINT
-
+sed 's/OKAHU_API_KEY=/OKAHU_API_KEY='"$1"'/g ; s/TRITON_LLM_ENDPOINT=/TRITON_LLM_ENDPOINT=http:\/\/127.0.0.1:8000\/v2\/models\/flan_t5\/infer/g ; s/TRITON_LLM_GRPC_ENDPOINT=/TRITON_LLM_GRPC_ENDPOINT=127.0.0.1:8001/g' config/config.ini.template > config/config.ini 
