e6d3e7855f4296cd8d56a607179536f8585900ab
prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>
2024-07-09 21:01:08 +0000

    Lower logging level to suppress Chroma warning

--- llama_index_triton/triton_llm_demo_with_okahu.py
+++ llama_index_triton/triton_llm_demo_with_okahu.py
@@ -2,12 +2,16 @@ import sys
 import triton_llm_demo
 from okahu_apptrace.instrumentor import setup_okahu_telemetry
 from credential_utilties.environment import setOkahuEnvironmentVariablesFromConfig
+import logging
+
 
 def main():
     #Okahu instrumentation
     setOkahuEnvironmentVariablesFromConfig(sys.argv[1])
     setup_okahu_telemetry(workflow_name="llama_triton_wf4")
 
+    logger = logging.getLogger()
+    logger.setLevel(logging.ERROR)
     #invoke the underlying application
     triton_llm_demo.main()
 
